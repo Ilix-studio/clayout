700f2088f1aa10dc32c5abfdc71e377f119848fe
Ilix-studio <ilishjyoti17@gmail.com>
2022-04-03 02:55:44 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`))
+
+//call mongodb connection function
+connectDB();
 
 app.get('/', (req, res) => {
     res.status(200).send(`Routing is happening`)
