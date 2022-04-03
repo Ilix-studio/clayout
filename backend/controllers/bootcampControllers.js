1f59d46b69759a67cfdffc2842e4f249cc9c22f4
Ilix-studio <ilishjyoti17@gmail.com>
2022-04-03 09:46:08 +0530

    made validation schema models

--- backend/controllers/bootcampControllers.js
+++ backend/controllers/bootcampControllers.js
@@ -1,15 +1,30 @@
-exports.getAllBootcamps = (req, res, next) => {
-    res.send("Get all bootcamps");
+const Bootcamp = require('./../models/BootcampsModels');
+
+// find function is used to send a query to mongoDB to retrive all the bootcamp.
+// if not error comes
+// So, Global Error Handling System or asynchorous handler
+// So, we have to used trycatch block in every singlr controller 
+// it makes development easier
+// Getting your errors in better format
+// So, middleware is used to handle it.
+
+//controller1
+exports.getAllBootcamps = async (req, res, next) => {
+   const bootcamp = await Bootcamp.find();
+  
 }
 
+//controller2
 exports.createBootcamp = (req, res, next) => {
     res.send("Create  bootcamps");
 }
 
+//controller3
 exports.updateBootcampById = (req, res, next) => {
     res.send("update a bootcamp by id");
 }
 
+//controller4
 exports.deleteBootcampById = (req, res, next) => {
     res.send("Delete a bootcamps by id");
 }
---
+++ backend/models/BootcampsModels.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const bootcampSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [ true, 'Please write a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        required: [true.valueOf, 'Please provide rating'],
+        unique: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Please provide the required description'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Enter price'] 
+    }
+})
+
+const Bootcamp = mongoose.model('Bootcamp', bootcampSchema);
+
+module.exports = Bootcamp;
