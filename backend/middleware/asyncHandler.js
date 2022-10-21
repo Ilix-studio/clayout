03c63bdc30ee9f7b725584d5f5b1ec142b9cbc74
Ilix-studio <ilishjyoti17@gmail.com>
2022-10-21 08:33:07 +0530

    handling error

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,8 +1,11 @@
-// promise all 
+// promise all
 
 const asyncHandler = (controllerfunction) => (req, res, next) =>
- Promise.resolve(controllerfunction(req, res, next)).catch(next);
+  Promise.resolve(controllerfunction(req, res, next)).catch(next);
 
- module.exports=asyncHandler;
+module.exports = asyncHandler;
 
- //import to bootcampController.js
+//import to bootcampController.js
+
+//Anytime you work with async await, some promise need to be return.
+// to avoid using error handling,aka- midddleware functionality,  use try catch block on every controller routes
