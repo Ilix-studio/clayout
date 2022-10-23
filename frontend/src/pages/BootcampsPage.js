16df8ed382bfc1e4312b40526c4679fdabe548df
Ilix-studio <ilishjyoti17@gmail.com>
2022-10-23 10:28:53 +0530

    data is fetch from backend

--- frontend/src/pages/BootcampsPage.js
+++ frontend/src/pages/BootcampsPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const BootcampsPage = () => {
-  return (
-    <div>BootcampsPage</div>
-  )
-}
+  //Component State
+  const [bootcamps, setBootcamps] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default BootcampsPage
+  //Side Effect
+  useEffect(() => {
+    let cancel;
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: "/api/v1/bootcamps",
+          cancelToken: new axios.CancelToken((c) => (cancel = c)),
+        });
+        setBootcamps(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>HElloo</div>;
+};
+
+export default BootcampsPage;
